3584ba14f2122fcc6f3a48520a125515910ae59b
hamzamashad <mashadhamza@outlook.com>
2022-10-13 22:29:38 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,51 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+// Callback to debug server running
+function listening() {
+    console.log('server is running');
+    console.log(`port is ${port}`);
+};
+
+// Data array
+const data = [];
+
+// Get route
+app.get('/all', (req, res) => {
+    res.send(data);
+});
+
+// POST route
+app.post('/weatherData', (req, res) => {
+    let newData = req.body;
+    let newEntry = {
+        temperature: newData.temperature,
+        date: newData.date,
+        userResponse: newData.userResponse,
+    }
+    data.push(newEntry);
+    console.log(data);
+})
---
+++ website/app.js
@@ -0,0 +1,114 @@
+/* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?lat=',
+zipURL = 'http://api.openweathermap.org/geo/1.0/zip?zip='
+apiKey = 'd3fdaa4d0905b18d74f519c2150e4320';
+const zipArea = document.getElementById('zip'),
+userFeelingsArea = document.getElementById('feelings'),
+dateDisplay = document.getElementById('date'),
+tempDisplay = document.getElementById('temp'),
+feelingsDisplay = document.getElementById('content');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction() {
+    const zipCode = zipArea.value,
+    userFeelings = userFeelingsArea.value;
+    const targetLocation = getGeo(zipURL, zipCode, apiKey);
+    const targetLat = getLat(targetLocation);
+    const targetLon = getLon(targetLocation);
+    const target = {
+        lat: targetLat,
+        lon: targetLon
+    };
+    const targetTemperature = getTemp(baseURL, target.lat, target.lon, apiKey);
+    const sendData = targetTemperature.then(function(data) {
+        postData('/weatherData', {
+            temperature: data,
+            date: newDate,
+            userResponse: userFeelings
+        })
+    });
+    sendData.then(updateUI);
+}
+
+/* Function to GET Web API Data*/
+const getGeo = async (url, zip, key) => {
+    const res = await fetch(url+zip+'&appid='+key);
+    try {
+        const data = await res.json();
+        return await data
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+const getTemp = async (url, lat, lon, key) => {
+    const res = await fetch(url+await lat+'&lon='+await lon+'&units=metric&appid='+key);
+    try {
+        const data = await res.json();
+        tempData = data.main.temp;
+        return tempData
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+
+/* Functions to process Project Data */
+const getLat = async (location) => {
+    const res = await location;
+    try {
+        const data = await res.lat;
+        return data
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+const getLon = async (location) => {
+    const res = await location;
+    try {
+        const data = await res.lon;
+        return data
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+      const response = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data)
+    });
+    
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData
+    } catch(error) {
+      console.log("error", error);
+    }
+}
+
+/* Function to GET Project Data */
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        tempDisplay.innerText = allData[0].temperature;
+        dateDisplay.innerText = allData[0].date;
+        feelingsDisplay.innerText = allData[0].userResponse;
+  
+    } catch(error) {
+        console.log("error", error);
+    }
+}
